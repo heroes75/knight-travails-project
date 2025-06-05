2e2d6d3a718574e148d6d14798becfd5604319c8
heroes75 <123456789+odin@users.noreply.github.com>
2025-06-05 17:24:33 +0000

    start after sick

---
+++ knight-travail.js
@@ -0,0 +1,23 @@
+class Node {
+    constructor(value, one = null, two = null, three = null, four = null, five = null, six = null, seven = null, eight = null) {
+        this.value = value;
+        this.one = one;
+        this.two = two;
+        this.three = three;
+        this.four = four;
+        this.five = five;
+        this.six = six;
+        this.seven = seven;
+        this.eight = eight;
+    }
+}
+
+class Graph {
+    constructor() {
+        this.root;
+    }
+
+    buildGraph(value) {
+        
+    }
+}
