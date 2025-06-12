91ca9793b022ca5360135b144b7b45d13e20733b
heroes75 <123456789+odin@users.noreply.github.com>
2025-06-12 06:43:47 +0000

    need help

--- knight-travail.js
+++ knight-travail.js
@@ -17,75 +17,29 @@ class Graph {
         this.root;
     }
 
+    // try to buid a graph of all possible nodes between this two value
     buildGraph(value, value1) {
         this.root = this.createNode([[9,9]], value, value1, [])
     }
 
+    //create helper function
+    //- prev: don't care about it it'sfor visualize the function
+    //- value: first value
+    //- value1: value to reach
+    //- all: history of precedent value reach by the knight
     createNode(prev, value, value1, all) {
         console.log("herre starat tg recusrsion");
         
-        console.log(all);
-        console.log({prev, value});
+        //console.log(all);
+        //console.log({prev, value});
         
+        // two base case:
+        //-the knight is out of board so i return null
+        //- or the value1 is reach so i return this node
         if(value[0] < 0 || value[1] < 0 || value[0] > 7 || value[1] > 7) return null
         if((value[0] === value1[0]) && (value[1] === value1[1])) return new Node(value)
-        //let node = new Node(value);
         
-        // two moves left and one move down or left
-        /*node.one = ((value[0] + 2 < 8) && (value[1] + 1 < 8)) ? this.createNode([value[0] + 2, value[1] + 1], value1) : null;
-        node.two = ((value[0] + 2 < 8) && (value[1] - 1 >= 0)) ? this.createNode([value[0] + 2, value[1] - 1], value1) : null;
-        // two moves right and one move down or left
-        node.three = ((value[0] - 2 >= 0) && (value[1] + 1 < 8)) ? this.createNode([value[0] - 2, value[1] + 1], value1) : null;
-        node.four = ((value[0] - 2 >= 0) && (value[1] - 1 >= 0)) ? this.createNode([value[0] - 2, value[1] - 1], value1) : null;
-        // two moves up and and one move left or right
-        node.five = ((value[0] + 1 < 8) && (value[1] + 2 < 8)) ? this.createNode([value[0] + 1, value[1] + 2], value1) : null;
-        node.six = ((value[0] - 1 >= 0) && (value[1] + 2 < 8)) ? this.createNode([value[0] - 1, value[1] + 2], value1) : null;
-        // two moves down and one move left or right
-        node.seven = ((value[0] + 1 < 8) && (value[1] - 2 >= 0)) ? this.createNode([value[0] + 1, value[1] - 2], value1) : null;
-        node.eight = ((value[0] - 1 >= 0) && (value[1] - 2 >= 0)) ? this.createNode([value[0] - 1, value[1] - 2], value1) : null;*/
-
-        /*if ((value[0] + 2 < 8) && (value[1] + 1 < 8)) {
-            node.one = new Node([value[0] + 2, value[1] + 1]);
-            this.createNode([value[0] + 2, value[1] + 1], value1)
-        } else {
-            node.one = null
-        }
-        if((value[0] + 2 < 8) && (value[1] - 1 >= 0)) {
-            node.two = new Node([value[0] + 2, value[1] - 1])
-        } else {
-            node.two = null
-        }
-        if((value[0] - 2 >= 0) && (value[1] + 1 < 8)) {
-            node.three = new Node([value[0] - 2, value[1] + 1]) 
-        } else {
-            node.three = null
-        }
-        if((value[0] - 2 >= 0) && (value[1] - 1 >= 0)) {
-            node.four = new Node([value[0] - 2, value[1] - 1])
-        } else {
-            node.four = null
-        }
-        if((value[0] + 1 < 8) && (value[1] + 2 < 8)) {
-            node.five = new Node([value[0] + 1, value[1] + 2])
-        } else {
-            node.five = null
-        }
-        if((value[0] - 1 >= 0) && (value[1] + 2 < 8)) {
-            node.six = new Node([value[0] - 1, value[1] + 2])
-        } else {
-            node.six = null
-        }
-        if((value[0] + 1 < 8) && (value[1] - 2 >= 0)) {
-            node.seven = new Node([value[0] + 1, value[1] - 2])
-        } else {
-            node.seven = null
-        }
-        if((value[0] - 1 >= 0) && (value[1] - 2 >= 0)) {
-            node.eight = new Node([value[0] - 1, value[1] - 2])
-        } else {
-            node.eight = null
-        }*/
-        
+        // list of futur values
         const arrayOfValues = [
             [value[0] + 2, value[1] + 1],
             [value[0] + 2, value[1] - 1],
@@ -97,6 +51,8 @@ class Graph {
             [value[0] - 1, value[1] - 2],
         ]
 
+        //filtered all values of future values to eleminate the case that the
+        // knig. already passed to prevent the infinite loop
         const arrayOfValuesFilter = arrayOfValues.filter(el => {
             if(all.length === 0) return true
             let isIn = true;
@@ -105,15 +61,11 @@ class Graph {
             });
             return isIn
         });
-        console.log(all.map(el => el.join()).join("||"), {arrayOfValuesFilter, arrayOfValues, prev, value});
+        //console.log(all.map(el => el.join()).join("||"), {arrayOfValuesFilter, arrayOfValues, prev, value});
         
-        for (let i = 0; i < arrayOfValuesFilter.length; i++) {
-            console.log(arrayOfValuesFilter[i]);
-            
-            
-        }
-        
+        // push value in the history of value
         if(!all.includes(value)) all.push(value);
+        
         const all1 = all
         const all2 = all
         const all3 = all
@@ -121,17 +73,14 @@ class Graph {
         const all5 = all
         const all6 = all
         const all7 = all
-        console.log(all.length, all2.length, all3.length, all4.length, all5.length, all6.length, all7.length);
+        //console.log(all.length, all2.length, all3.length, all4.length, all5.length, all6.length, all7.length);
         
         prev = value;
-        //console.log({prev}, [value[0] + 2, value[1] + 1]);
-        //console.log({prev}, [value[0] + 2, value[1] - 1]);
-        //console.log({prev}, [value[0] - 2, value[1] + 1]);
-        //console.log({prev}, [value[0] - 2, value[1] - 1]);
-        //console.log({prev}, [value[0] + 1, value[1] + 2]);
-        //console.log({prev}, [value[0] - 1, value[1] + 2]);
-        //console.log({prev}, [value[0] + 1, value[1] - 2]);
-        //console.log({prev}, [value[0] - 1, value[1] - 2]);
+        // my problem is here the modification made in the first invokation of
+        //arrayOfValuesFilter[0] to the history of case(all) is passed to 
+        // the following invokation(so the final result some node stop wihout reasons)
+        // now i solve this problem with Array.from(new Set(all1)) the function 
+        // seems a infinite loop. i need help to solve the problem
         let root = new Node(value, 
             arrayOfValuesFilter.length >= 1 ? this.createNode(prev, arrayOfValuesFilter[0], value1, Array.from(new Set(all1))) : null,
             arrayOfValuesFilter.length >= 2 ? this.createNode(prev, arrayOfValuesFilter[1], value1, Array.from(new Set(all2))) : null,
@@ -141,18 +90,22 @@ class Graph {
             arrayOfValuesFilter.length >= 6 ? this.createNode(prev, arrayOfValuesFilter[5], value1, Array.from(new Set(all6))) : null,
             arrayOfValuesFilter.length >= 7 ? this.createNode(prev, arrayOfValuesFilter[6], value1, Array.from(new Set(all7))) : null,
         )
-        console.log(root.value);
+
+        //console.log({all});
+        console.log(all.length);
         return root
+
+        
     }
 
-
+    
 }
 
 let graph = new Graph()
 
-graph.buildGraph([0,0], [7,7])
+//graph.buildGraph([0,0], [7,7])
 let a = [1,1]
 let b = [1,1]
 console.log(a[0] === b[0] && a[1] === b[1]);
-
-console.log("graph.root)", graph.root);
+graph.knightMoves([0,0], [2,1])
+console.log("graph.root", graph.root);
