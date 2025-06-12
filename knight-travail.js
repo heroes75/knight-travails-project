e72236aba7b298474b69f9640aa6167e6bf37039
heroes75 <123456789+odin@users.noreply.github.com>
2025-06-12 21:43:35 +0000

    try a new method

--- knight-travail.js
+++ knight-travail.js
@@ -23,7 +23,7 @@ class Graph {
     }
 
     //create helper function
-    //- prev: don't care about it it'sfor visualize the function
+    //- prev: don't care about it it's for visualize the function
     //- value: first value
     //- value1: value to reach
     //- all: history of precedent value reach by the knight
@@ -77,7 +77,7 @@ class Graph {
         
         prev = value;
         // my problem is here the modification made in the first invokation of
-        //arrayOfValuesFilter[0] to the history of case(all) is passed to 
+        // arrayOfValuesFilter[0] to the history of case(all) is passed to 
         // the following invokation(so the final result some node stop wihout reasons)
         // now i solve this problem with Array.from(new Set(all1)) the function 
         // seems a infinite loop. i need help to solve the problem
@@ -98,12 +98,96 @@ class Graph {
         
     }
 
+    knightMoves(value, value1) {
+        this.root = this.reachNode(false, value, value1, [])
+    }
+
+    //create helper function
+    //- prev: don't care about it it's for visualize the function
+    //- value: first value
+    //- value1: value to reach
+    //- all: history of precedent value reach by the knight
+    reachNode(find, value, value1, path) {
+        console.log("herre starat tg recusrsion");
+        
+        //console.log(all);
+        //console.log({prev, value});
+        
+        // two base case:
+        //-the knight is out of board so i return null
+        //- or the value1 is reach so i return this node
+        if(value[0] < 0 || value[1] < 0 || value[0] > 7 || value[1] > 7) return null
+        if((value[0] === value1[0]) && (value[1] === value1[1])) {
+            find = true;
+            path.push(value)
+            return path
+        }
+        
+        // list of futur values
+        const arrayOfValues = [
+            [value[0] + 2, value[1] + 1],
+            [value[0] + 2, value[1] - 1],
+            [value[0] - 2, value[1] + 1],
+            [value[0] - 2, value[1] - 1],
+            [value[0] + 1, value[1] + 2],
+            [value[0] - 1, value[1] + 2],
+            [value[0] + 1, value[1] - 2],
+            [value[0] - 1, value[1] - 2],
+        ]
+
+        //filtered all values of future values to eleminate the case that the
+        // knig. already passed to prevent the infinite loop
+        const arrayOfValuesFilter = arrayOfValues.filter(el => {
+            if(path.length === 0) return true
+            let isIn = true;
+            path.forEach(prevs => {
+                isIn = isIn && ((el[0] !== prevs[0]) || (el[1] !== prevs[1]));
+            });
+            return isIn
+        });
+        //console.log(all.map(el => el.join()).join("||"), {arrayOfValuesFilter, arrayOfValues, prev, value});
+        
+        // push value in the history of value
+        if(!path.includes(value)) path.push(value);
+        
+        const all1 = path
+        const all2 = path
+        const all3 = path
+        const all4 = path
+        const all5 = path
+        const all6 = path
+        const all7 = path
+        //console.log(all.length, all2.length, all3.length, all4.length, all5.length, all6.length, all7.length);
+        
+        //prev = value;
+        // my problem is here the modification made in the first invokation of
+        // arrayOfValuesFilter[0] to the history of case(all) is passed to 
+        // the following invokation(so the final result some node stop wihout reasons)
+        // now i solve this problem with Array.from(new Set(all1)) the function 
+        // seems a infinite loop. i need help to solve the problem
+        let root = new Node(value, 
+            arrayOfValuesFilter.length >= 1 && find !== true  ? this.createNode(find, arrayOfValuesFilter[0], value1, all1) : null,
+            arrayOfValuesFilter.length >= 2 && find !== true  ? this.createNode(find, arrayOfValuesFilter[1], value1, all2) : null,
+            arrayOfValuesFilter.length >= 3 && find !== true  ? this.createNode(find, arrayOfValuesFilter[2], value1, all3) : null,
+            arrayOfValuesFilter.length >= 4 && find !== true  ? this.createNode(find, arrayOfValuesFilter[3], value1, all4) : null,
+            arrayOfValuesFilter.length >= 5 && find !== true  ? this.createNode(find, arrayOfValuesFilter[4], value1, all5) : null,
+            arrayOfValuesFilter.length >= 6 && find !== true  ? this.createNode(find, arrayOfValuesFilter[5], value1, all6) : null,
+            arrayOfValuesFilter.length >= 7 && find !== true  ? this.createNode(find, arrayOfValuesFilter[6], value1, all7) : null,
+        )
+
+        //console.log({all});
+        console.log(path.length);
+        return find === true ? path : root
+
+        
+    }
+
     
 }
 
 let graph = new Graph()
 
-graph.buildGraph([0,0], [7,7])
+graph.knightMoves([0,0], [7,7])
 let a = [1,1]
 let b = [1,1]
 console.log(a[0] === b[0] && a[1] === b[1]);
